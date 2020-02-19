8f41619cecb9de2a34875ee4720eb2ee98a6221a
devpatna <devpatna@LIN37000989.corp.capgemini.com>
2020-02-19 18:30:49 +0530

    Intial Commit the Spring Boot Backend application and configuration done

---
+++ online-bookapp/src/main/java/com/spain/csd/OnlineBookappApplication.java
@@ -0,0 +1,14 @@
+package com.spain.csd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookappApplication.class, args);
+		System.out.println("main called =======");
+	}
+
+}
