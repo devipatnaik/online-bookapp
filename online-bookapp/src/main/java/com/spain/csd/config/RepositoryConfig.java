a9db4ff2491056d185eb0dedf4e2adf150e4d776
devpatna <devpatna@LIN37000989.corp.capgemini.com>
2020-02-20 14:37:04 +0530

    JSON Response 'ID' populated for All Entity.

--- online-bookapp/src/main/java/com/spain/csd/config/RepositoryConfig.java
+++ online-bookapp/src/main/java/com/spain/csd/config/RepositoryConfig.java
@@ -4,13 +4,12 @@
 package com.spain.csd.config;
 
 import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-
-import com.spain.csd.entity.Book;
 
 /**
  * @author devpatna
@@ -25,7 +24,14 @@ public class RepositoryConfig implements RepositoryRestConfigurer {
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 		
-		config.exposeIdsFor(Book.class);
+		//config.exposeIdsFor(Book.class);
+		//config.exposeIdsFor(BookCategory.class);
+		
+		// Get the ID(JSON Response) for all the Entity classes.
+		config.exposeIdsFor(entityManager.getMetamodel().getEntities()
+				.stream()
+				.map(Type :: getJavaType)
+				.toArray(Class[] :: new)
+				);
 	}
-	
 }
