98cb2fe1ab4b1b4d8bff79496da04b4b9a341a63
Patnaik <devi.patnaik@capgemini.com>
2020-02-20 11:40:11 +0530

    Angular Components Created!

---
+++ angular-bookapp/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/common/book';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  constructor() { }
+
+  books: Book[] = [
+    {
+          sku: 'Text-100',
+          name: 'C Programming Language',
+          description: 'Learn C Programming Language',
+          unitPrice: 500,
+          imageUrl: 'assets/images/books/text-100.png',
+          active: true,
+          unitsInStock: 100,
+          createdOn: new Date(),
+          updatedOn: null,
+    },
+    {
+          sku: 'Text-105',
+          name: 'Python Programming Language',
+          description: 'Learn Python Programming Language',
+          unitPrice: 800,
+          imageUrl: 'assets/images/books/text-105.png',
+          active: true,
+          unitsInStock: 150,
+          createdOn: new Date(),
+          updatedOn: null,
+    },
+    {
+          sku: 'Text-110',
+          name: 'Java8 Programming Language',
+          description: 'Learn Java8 Programming Language',
+          unitPrice: 400,
+          imageUrl: 'assets/images/books/text-110.png',
+          active: true,
+          unitsInStock: 250,
+          createdOn: new Date(),
+          updatedOn: null,
+    }
+      ];
+
+  ngOnInit() {
+  }
+
+}
