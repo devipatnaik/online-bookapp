bbae1ec6ead37221b38bea955a2df6e7a3652649
devpatna <devpatna@LIN37000989.corp.capgemini.com>
2020-02-20 21:52:05 +0530

    Backend to Angular Connected and Completed Module-1

--- online-bookapp/src/main/java/com/spain/csd/repository/BookRepository.java
+++ online-bookapp/src/main/java/com/spain/csd/repository/BookRepository.java
@@ -4,7 +4,6 @@
 package com.spain.csd.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.spain.csd.entity.Book;
@@ -13,7 +12,6 @@ import com.spain.csd.entity.Book;
  * @author devpatna
  *
  */
-@Repository
 @CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
